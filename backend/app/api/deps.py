e70add586eae08a1b16f3b06c809ff73aae60bec
Versacext <zajnelovn@gmail.com>
2026-05-18 14:47:27 +0500

    Create deps.py

---
+++ backend/app/api/deps.py
@@ -0,0 +1,37 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+import jwt
+from sqlalchemy.orm import Session
+
+from app.core.config import settings
+from app.core.database import get_db
+from app.models.models import User
+from app.schemas.auth import TokenData
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/v1/auth/login")
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)) -> User:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Не удалось валидировать учетные данные",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, settings.JWT_SECRET, algorithms=[settings.ALGORITHM])
+        user_id: str = payload.get("user_id")
+        role: str = payload.get("role")
+        if user_id is None:
+            raise credentials_exception
+        token_data = TokenData(user_id=user_id, role=role)
+    except jwt.PyJWTError:
+        raise credentials_exception
+        
+    user = db.query(User).filter(User.id = token_data.user_id).first()
+    if user is None:
+        raise credentials_exception
+    return user
+
+def get_current_admin(current_user: User = Depends(get_current_user)) -> User:
+    if current_user.role != "admin":
+        raise HTTPException(status_code=403, detail="Недостаточно прав. Доступно только для Администратора")
+    return current_user
